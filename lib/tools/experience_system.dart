4025677fa3be6c65dc2d830a6383d154d5246e62
chewu <zheliuye@gmail.com>
2023-02-15 15:57:18 +0800

    floor

--- lib/tools/experience_system.dart
+++ lib/tools/experience_system.dart
@@ -2,7 +2,7 @@
 class Experience{
   static int calculate_Currentlevel(experience){
     print(experience);
-    return (experience/10).round();
+    return (experience/10).floor();
   }
   static int calculate_LastExperience(experience){
     
