689200ce6aeb0a659e4a4f259fc0e9c0bcd3c1ce
bbnoir <bbnoir83@gmail.com>
2023-03-22 12:45:17 +0800

    support notify

--- lib/main.dart
+++ lib/main.dart
@@ -5,46 +5,14 @@ import 'package:recycle_app/models/myuser.dart';
 import 'package:recycle_app/screen/wrapper.dart';
 import 'package:recycle_app/service/auth.dart';
 import 'firebase_options.dart';
-import 'package:firebase_messaging/firebase_messaging.dart';
+import 'package:recycle_app/service/notify.dart';
 //import 'package:provider/provider.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
-  /*
-  final fcmToken = await FirebaseMessaging.instance.getToken();
-  FirebaseMessaging.instance.onTokenRefresh.listen((fcmToken) {
-    // TODO: If necessary send token to application server.
-
-    // Note: This callback is fired at each app startup and whenever a new
-    // token is generated.
-  }).onError((err) {
-    // Error getting token.
-  });
-  */
-  FirebaseMessaging messaging = FirebaseMessaging.instance;
-
-  NotificationSettings settings = await messaging.requestPermission(
-    alert: true,
-    announcement: false,
-    badge: true,
-    carPlay: false,
-    criticalAlert: false,
-    provisional: false,
-    sound: true,
-  );
-
-  print('User granted permission: ${settings.authorizationStatus}');
-
-  FirebaseMessaging.onMessage.listen((RemoteMessage message) {
-    print('Got a message whilst in the foreground!');
-    print('Message data: ${message.data}');
-
-    if (message.notification != null) {
-      print('Message also contained a notification: ${message.notification}');
-    }
-  });
-  runApp(const MyApp());
+  initNotify();
+   runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
