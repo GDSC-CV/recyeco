8e09dabc47a6ba5a07a6492050a3e6424abb1f57
chewu <zheliuye@gmail.com>
2023-03-29 03:27:12 +0800

    Add friend list and Request list

--- lib/screen/home_page/friend_page.dart
+++ lib/screen/home_page/friend_page.dart
@@ -17,14 +17,87 @@ class _FriendPageState extends State<FriendPage> {
   
   final _formKey = GlobalKey<FormState>();
   String _friendId = "";
-
+  bool refresh = false;
   @override
   Widget build(BuildContext context) {
 
     UserData userData = Provider.of<UserData>(context);
-
+    List userFriends = userData.friends;
+    
     return Scaffold(
-      appBar: AppBar(),
+        appBar: AppBar(),
+        backgroundColor: Colors.blueGrey,
+        body: Column(
+          children: [
+            SizedBox(height: 50,),
+            ElevatedButton(
+              onPressed:()async{
+                
+                bool message = await Navigator.of(context).push(
+                  MaterialPageRoute(
+                    builder: (BuildContext context) => Provider(
+                      create: (context) => userData,
+                      builder: (context, child) => const AddFriendPage(),
+                    ),
+                  ),
+                );
+                
+                if(message==true){
+                  
+                  setState(() {
+                    
+                  });
+                }
+              }, 
+              child: Text("Add friend")
+            ),
+            Expanded(
+              
+              
+              child: ListView.builder(
+                padding: const EdgeInsets.all(8),
+                itemCount: userFriends.length,
+                itemBuilder: (BuildContext context, int index) {
+                  return Container(
+                    height: 50,
+                    color: Colors.blueGrey,
+                    child: Center(child: Text(userFriends[index])),
+                  );
+                }
+              ),
+            ),
+            
+          ]
+        )
+      );
+    
+  }
+}
+
+//add Friend Page
+class AddFriendPage extends StatefulWidget {
+  const AddFriendPage({super.key});
+
+  @override
+  State<AddFriendPage> createState() => _AddFriendPageState();
+}
+
+class _AddFriendPageState extends State<AddFriendPage> {
+  final _formKey = GlobalKey<FormState>();
+  String _friendId = "";
+  @override
+  Widget build(BuildContext context) {
+    UserData userData = Provider.of<UserData>(context);
+    List userFriendRequests = userData.friendRequests;
+    return Scaffold(
+      appBar: AppBar(
+        leading: BackButton(
+          onPressed: (){
+            
+            Navigator.pop(context,true);
+          },
+        ),
+      ),
       backgroundColor: Colors.grey[300],
       body: Form(
         key: _formKey,
@@ -101,6 +174,40 @@ class _FriendPageState extends State<FriendPage> {
                
               
             ),
+            Expanded(
+              child: ListView.builder(
+                padding: const EdgeInsets.all(8),
+                itemCount: userFriendRequests.length,
+                itemBuilder: (BuildContext context, int index) {
+                  return Container(
+                    //height: 50,
+                    color: Colors.blueGrey,
+                    child: Row(
+                      children: [
+                        SizedBox(width: 20,),
+                        Text(
+                          userFriendRequests[index]
+                        ),
+                        SizedBox(width: 20,),
+                        IconButton(onPressed: (){
+                          FriendSystem.acceptFriend(userData, userFriendRequests[index]);
+                          FriendSystem.DeleteFriendRequest(userData, userFriendRequests[index]);
+                          setState(() {
+                            
+                          });
+                        }, icon: Icon(Icons.check)),
+                        IconButton(onPressed: (){
+                          FriendSystem.DeleteFriendRequest(userData, userFriendRequests[index]);
+                          setState(() {
+                            
+                          });
+                        }, icon: Icon(Icons.cancel_outlined))
+                      ],
+                    ),
+                  );
+                }
+              ),
+            )
             
           ],
         )
--- lib/tools/friend_system.dart
+++ lib/tools/friend_system.dart
@@ -10,6 +10,7 @@ class FriendSystem{
     currentUserFriendList.add(uid);
     await DatabaseService(uid:currentUserData.uid).updateUserDataFriend(currentUserFriendList);
     var otherUserDataStream = DatabaseService(uid: uid).userData;
+    
     UserData otherUserData;
     List friendRequests = List.empty(growable: true);
     otherUserDataStream.listen(
@@ -25,4 +26,41 @@ class FriendSystem{
     friendRequests.add(currentUserData.uid);
     return await DatabaseService(uid: uid).updateUserDataFriendRequest(friendRequests);
   }
+  static Future acceptFriend(UserData currentUserData,String uid)async{
+    var currentUserFriendList = currentUserData.friends;
+    currentUserFriendList.add(uid);
+    return await DatabaseService(uid:currentUserData.uid).updateUserDataFriend(currentUserFriendList);
+    
+  }
+  static Future DeleteFriendRequest(UserData currentUserData,String uid)async{
+    var currentUserFriendRequestList = currentUserData.friendRequests;
+    currentUserFriendRequestList.remove(uid);
+    await DatabaseService(uid:currentUserData.uid).updateUserDataFriendRequest(currentUserFriendRequestList);
+    
+  }
+  // static Future getRankedFriend(UserData currentUserData)async{
+  //   var currentUserFriendList = currentUserData.friends;
+  //   int row = currentUserFriendList.length;
+
+  //   var twoDList = List.generate(row, (i) => List.of(["name",0]), growable: false);
+  //   for (var i = 0; i < twoDList.length; i++) {
+  //     var otherUserDataStream = DatabaseService(uid: currentUserFriendList[i]).userData;
+  //     UserData otherUserData=UserData(uid: "1", name: "1", level: 1, experiences: 1, friends: List.empty(), friendRequests: List.empty());
+  //     otherUserDataStream.listen(
+  //       (event) {
+  //         otherUserData =  event;
+  //       },
+  //       onError: (err){
+  //         print(err);
+  //         return null;
+  //       }
+  //     );
+  //     twoDList[i][0] = otherUserData.name;
+  //     twoDList[i][1] = otherUserData.experiences;
+
+  //   }
+  //   twoDList.sort((x,y)=>(x[1] as dynamic).compareTo((y[1] as dynamic)));
+  //   return twoDList;
+    
+  // }
 }
